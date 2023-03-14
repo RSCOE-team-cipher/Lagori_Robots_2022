de69fb21082615973e5f5fdc0e4698e67f1b866b
Rituraj Navindgikar <74851393+rituraj-navindgikar@users.noreply.github.com>
2023-03-14 23:56:26 +0530

    Delete pico_ros2_2022_lagori_code directory

--- pico_ros2_2022_lagori_code/BACKUP_TRASH/captain_differential_code/libmicroros/include/lagori_robot_msgs/share/lagori_robot_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(lagori_robot_msgs_IDL_FILES "msg/StepperMsg.idl;msg/GripperMsg.idl;msg/RobotOneControls.idl")
-set(lagori_robot_msgs_INTERFACE_FILES "msg/StepperMsg.msg;msg/GripperMsg.msg;msg/RobotOneControls.msg")
